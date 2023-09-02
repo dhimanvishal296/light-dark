b5df59e9961c9883b2c6fb6d894f1c346b91ec1d
Vishal Dhiman <dhimanvishal296@gmail.com>
2023-09-03 01:08:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [theme, setTheme] = useState("light");
+
+    function toggleTheme() {
+        setTheme(theme === "light" ? "dark" : "light");
+    }
+
+    return (
+        <div className={theme === "light" ? "light" : "dark"} >
+            <h1>Little Lemon 🍕</h1>
+            <p className="para">ThemeProvider should render a context provider component and inject as the context value an object with two properties: a theme property that is a string, that can be either light or dark, and a function named toggleTheme that enables you to toggle the theme. The useTheme hook should return that context object.</p>
+            <button className="btn" onClick={toggleTheme}>Toggle theme</button>
+        </div>
+    );
 }
 
 export default App;
